464aeb7af9c8ce2373ae9ea3c2e0f72acc15afe3
bleusaphir <71881857+bleusaphir@users.noreply.github.com>
2021-04-26 14:10:03 +0200

    Test

--- faceDetection/contributed/GUI.py
+++ faceDetection/contributed/GUI.py
@@ -7,11 +7,22 @@ from PIL import ImageTk
 import real_time_face_recognition as rt
 import threading, queue
 import face
+import detect as dt
 #pip install tk
 #pip install python-opencv
 #pip install pillow
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
 
-
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+import argparse
+from pathlib import Path
 
 
 def Verification(txt,window):
@@ -76,12 +87,7 @@ def CamWindow():
 
     print("la version de cv2 : ", cv2.__version__)
 
-#   net = cv2.dnn.readNet(r"C:\Users\ifadd\Desktop\projetIA\face_recognition_FPMS\faceDetection\contributed\yolov4-custom.weights", r"C:\Users\ifadd\Desktop\projetIA\face_recognition_FPMS\faceDetection\contributed\yolov4-custom.cfg")
-#   net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-#   net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
 
-    model = None
-#   model.setInputParams(size=(416, 416), scale=1 / 255)
 
     #On capture la vidéo du périphérique 0 (webcam intégrée)
     cap = cv2.VideoCapture(0)
@@ -144,7 +150,7 @@ def CamWindow():
     #►►►► START ◄◄◄◄
     face_recognition = face.Recognition()
     #Tracking(root, cap, photo) #On l'update la première fois
-    rt.processFrame(root, cap, photo, face_recognition, q, model)
+    dt.detect()
     root.mainloop()
     
     #►►►► STOP ◄◄◄◄
@@ -152,5 +158,25 @@ def CamWindow():
     cap.release()
     
 if __name__ == "__main__":
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='testYaya.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='0', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    opt = parser.parse_args()
+    print(opt)
+    check_requirements(exclude=('pycocotools', 'thop'))
     CamWindow()
